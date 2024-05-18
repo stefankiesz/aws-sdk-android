bfa5c41eaa3ba57fde16ef0973e1776c4d6dd1c1
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-05-17 20:30:49 -0700

    Begin modularizing code, not yet built nor test run

--- aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/audioVideo/MicrophoneSource.java
+++ aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/audioVideo/MicrophoneSource.java
@@ -25,8 +25,10 @@ import static com.amazonaws.kinesisvideo.util.StreamInfoConstants.AUDIO_TRACK_ID
 
 
 
-public class MicrophoneFramesSource {
-    private static final String TAG = MicrophoneFramesSource.class.getSimpleName();
+public class MicrophoneSource {
+    private static final String TAG = MicrophoneSource.class.getSimpleName();
+
+    // TODO: Make the below configured through mAudioVideoMediaSourceConfiguration
     private static final int AUDIO_SAMPLE_RATE = 8000; // mMediaSourceConfiguration.getSampleRate(), Hardcoded sample rate for now (emulator requires 8000);
     private static final int AUDIO_CHANNEL_TYPE = AudioFormat.CHANNEL_IN_MONO; // Android Docs: "CHANNEL_IN_MONO is guaranteed to work on all devices."
     private static final int AUDIO_ENCODING_TYPE = AudioFormat.ENCODING_PCM_16BIT; // Android Docs: "ENCODING_PCM_16BIT is Guaranteed to be supported by devices."
@@ -36,6 +38,7 @@ public class MicrophoneFramesSource {
 
     private final OutputStream outputStream = new ByteArrayOutputStream();
 
+    private AudioVideoMediaSourceConfiguration mAudioVideoMediaSourceConfiguration;
 
     private AudioRecord audioRecord = null;
     MediaCodec audioEncoder = null;
@@ -48,7 +51,7 @@ public class MicrophoneFramesSource {
 
     private MediaSourceSink mMediaSourceSink;
 
-    public MicrophoneFramesSource(MediaSourceSink mediaSourceSink) {
+    public MicrophoneSource(MediaSourceSink mediaSourceSink) {
         mMediaSourceSink = mediaSourceSink;
     }
 
@@ -152,17 +155,6 @@ public class MicrophoneFramesSource {
 
         return mediaCodec;
     }
-
-    // private void encodeCapturedAudio(MediaCodec audioEncoder) {
-    //     int codecInputBufferIndex = audioEncoder.dequeueInputBuffer(10 * 1000);
-
-    //     if (codecInputBufferIndex >= 0) {
-    //         ByteBuffer codecBuffer = codecInputBuffers[codecInputBufferIndex];
-    //         codecBuffer.clear();
-    //         codecBuffer.put(audioRecordData);
-    //         audioEncoder.queueInputBuffer(codecInputBufferIndex, 0, length, 0, running ? 0 : audioEncoder.BUFFER_FLAG_END_OF_STREAM);
-    //     }
-    // }
 
     private boolean handleCodecInput(ByteBuffer[] codecInputBuffers, boolean running) throws IOException {
         byte[] audioRecordData = new byte[bufferSize];
@@ -300,205 +292,4 @@ public class MicrophoneFramesSource {
                         frameData,
                         AUDIO_TRACK_ID));
     }
-
-
-    // private void submitAudioSampleToKVS() {
-    //     // Write the output audio in byte
-
-    //     String filePath = "/sdcard/voice8K16bitmono.pcm";
-    //     short sData[] = new short[BufferElements2Rec];
-
-    //     FileOutputStream os = null;
-    //     try {
-    //         os = new FileOutputStream(filePath);
-    //     } catch (FileNotFoundException e) {
-    //         e.printStackTrace();
-    //     }
-
-    //     while (isRecording) {
-    //         // gets the voice output from microphone to byte format
-
-    //         recorder.read(sData, 0, BufferElements2Rec);
-    //         System.out.println("Short writing to file" + sData.toString());
-    //         try {
-    //             // // writes the data to file from buffer
-    //             // // stores the voice buffer
-    //             byte bData[] = short2byte(sData);
-    //             os.write(bData, 0, BufferElements2Rec * BytesPerElement);
-    //         } catch (IOException e) {
-    //             e.printStackTrace();
-    //         }
-    //     }
-    //     try {
-    //         os.close();
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
-    // private void sendEncodedFrameToProducerSDK(final ByteBuffer encodedData) {
-    //     final long currentTime = System.currentTimeMillis();
-    //     Log.d(TAG, "time between frames: " + (currentTime - mLastRecordedFrameTimestamp) + "ms");
-    //     mLastRecordedFrameTimestamp = currentTime;
-
-    //     if (mFragmentStart == 0) {
-    //         mFragmentStart = currentTime;
-    //     }
-
-    //     final ByteBuffer frameData = encodedData;
-
-    //     mFrameAvailableListener.onFrameAvailable(
-    //             FrameUtility.createFrame(
-    //                     mBufferInfo,
-    //                     1 + currentTime - mFragmentStart,
-    //                     mFrameIndex++,
-    //                     frameData));
-    // }
-
-
-
 }
-
-
-
-
-
-
-
-
-// public class Audio_Record extends Activity {
-//     private static final int RECORDER_SAMPLERATE = 8000;
-//     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
-//     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-//     private AudioRecord recorder = null;
-//     private Thread recordingThread = null;
-//     private boolean isRecording = false;
-
-//     @Override
-//     public void onCreate(Bundle savedInstanceState) {
-//         super.onCreate(savedInstanceState);
-//         setContentView(R.layout.main);
-
-//         setButtonHandlers();
-//         enableButtons(false);
-
-//         int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
-//                 RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING); 
-//     }
-
-//     private void setButtonHandlers() {
-//         ((Button) findViewById(R.id.btnStart)).setOnClickListener(btnClick);
-//         ((Button) findViewById(R.id.btnStop)).setOnClickListener(btnClick);
-//     }
-
-//     private void enableButton(int id, boolean isEnable) {
-//         ((Button) findViewById(id)).setEnabled(isEnable);
-//     }
-
-//     private void enableButtons(boolean isRecording) {
-//         enableButton(R.id.btnStart, !isRecording);
-//         enableButton(R.id.btnStop, isRecording);
-//     }
-
-//     int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
-//     int BytesPerElement = 2; // 2 bytes in 16bit format
-
-//     private void startRecording() {
-
-//         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-//                 RECORDER_SAMPLERATE, RECORDER_CHANNELS,
-//                 RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
-
-//         recorder.startRecording();
-//         isRecording = true;
-//         recordingThread = new Thread(new Runnable() {
-//             public void run() {
-//                 writeAudioDataToFile();
-//             }
-//         }, "AudioRecorder Thread");
-//         recordingThread.start();
-//     }
-
-//         //convert short to byte
-//     private byte[] short2byte(short[] sData) {
-//         int shortArrsize = sData.length;
-//         byte[] bytes = new byte[shortArrsize * 2];
-//         for (int i = 0; i < shortArrsize; i++) {
-//             bytes[i * 2] = (byte) (sData[i] & 0x00FF);
-//             bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
-//             sData[i] = 0;
-//         }
-//         return bytes;
-
-//     }
-
-//     private void writeAudioDataToFile() {
-//         // Write the output audio in byte
-
-//         String filePath = "/sdcard/voice8K16bitmono.pcm";
-//         short sData[] = new short[BufferElements2Rec];
-
-//         FileOutputStream os = null;
-//         try {
-//             os = new FileOutputStream(filePath);
-//         } catch (FileNotFoundException e) {
-//             e.printStackTrace();
-//         }
-
-//         while (isRecording) {
-//             // gets the voice output from microphone to byte format
-
-//             recorder.read(sData, 0, BufferElements2Rec);
-//             System.out.println("Short writing to file" + sData.toString());
-//             try {
-//                 // // writes the data to file from buffer
-//                 // // stores the voice buffer
-//                 byte bData[] = short2byte(sData);
-//                 os.write(bData, 0, BufferElements2Rec * BytesPerElement);
-//             } catch (IOException e) {
-//                 e.printStackTrace();
-//             }
-//         }
-//         try {
-//             os.close();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//     }
-
-//     private void stopRecording() {
-//         // stops the recording activity
-//         if (null != recorder) {
-//             isRecording = false;
-//             recorder.stop();
-//             recorder.release();
-//             recorder = null;
-//             recordingThread = null;
-//         }
-//     }
-
-//     private View.OnClickListener btnClick = new View.OnClickListener() {
-//         public void onClick(View v) {
-//             switch (v.getId()) {
-//             case R.id.btnStart: {
-//                 enableButtons(true);
-//                 startRecording();
-//                 break;
-//             }
-//             case R.id.btnStop: {
-//                 enableButtons(false);
-//                 stopRecording();
-//                 break;
-//             }
-//             }
-//         }
-//     };
-
-//     @Override
-//     public boolean onKeyDown(int keyCode, KeyEvent event) {
-//         if (keyCode == KeyEvent.KEYCODE_BACK) {
-//             finish();
-//         }
-//         return super.onKeyDown(keyCode, event);
-//     }
-// }
